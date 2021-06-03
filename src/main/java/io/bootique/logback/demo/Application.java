0d01327a54451f09ebe7582642f1a574d590162c
Dmitriy Tarasenko <dmitriy.tarasienko@gmail.com>
2021-06-03 10:47:43 +0300

    feat: upgrade bootique-bom to 2.0.B1 and move to separate branch

--- src/main/java/io/bootique/logback/demo/Application.java
+++ src/main/java/io/bootique/logback/demo/Application.java
@@ -1,17 +1,17 @@
 package io.bootique.logback.demo;
 
+import io.bootique.di.BQModule;
+import io.bootique.di.Binder;
 import io.bootique.logback.demo.debug.DebugCommand;
 import io.bootique.logback.demo.error.ErrorCommand;
 import io.bootique.logback.demo.info.InfoCommand;
 import io.bootique.logback.demo.trace.TraceCommand;
 import io.bootique.logback.demo.warn.WarnCommand;
-import com.google.inject.Binder;
-import com.google.inject.Module;
 import io.bootique.BQCoreModule;
 import io.bootique.Bootique;
 import io.bootique.command.CommandDecorator;
 
-public class Application implements Module {
+public class Application implements BQModule {
 
     public static void main(String[] args) {
         Bootique
